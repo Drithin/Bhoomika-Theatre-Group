80209b43a1660c4a2edc560e9cc83a997f2abef2
Drithin <drithn@gmail.com>
2020-02-19 19:11:50 +0530

    edited and modified php code for contact form. added new thank you page for whoever submitted the contact form and styled it. figuring out form validation is next step

--- contact-send.php
+++ contact-send.php
@@ -15,7 +15,7 @@
 <body style='background-color:#f0f0f0; padding:40px 0;'>
 <table align='center' cellpadding='10' bgcolor='#FFFFFF' cellspacing='0' border='0' bordercolor='#ccc' style='width:100%; max-width:600px; font-family:Arial, Helvetica, sans-serif; font-size:13px; border:3px solid #e2e2e2;'>
 	<tr>
-    	<td colspan='2' align='center' style='font-size:25px; font-family:'Trebuchet MS', Arial, Helvetica, sans-serif;'><h2>Arrant Pharma Contact Details<h2></td>
+    	<td colspan='2' align='center' style='font-size:25px; font-family:'Trebuchet MS', Arial, Helvetica, sans-serif;'><h2> Contact Enquiry from Bhoomika Website<h2></td>
     </tr>
     <tr>
     	<td width='180' valign='top' style='border-right:1px solid #e2e2e2;border-bottom:1px solid #e2e2e2; border-top:1px solid #e2e2e2;'>Name</td>
@@ -42,6 +42,6 @@
 	$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
 	$headers .= 'From: ' ."$email". "\r\n";
 	mail($to,$subject,$message,$headers);
-	header("Location:index.html");
+	header("Location:contact-thankyou.html");
 
 ?>
